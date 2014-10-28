88fe4b3a8a5abce155de8ab766568c7dceaa453e
Pam Gaiguer <pamella.gaiguer@gmail.com>
2014-10-27 23:13:01 -0200

    starting db and crud for dates

---
+++ db/Create_Tables.sql
@@ -0,0 +1,27 @@
+use noturnall;
+
+create table users(
+    usr_id int auto_increment,
+    usr_name varchar(255) not null,
+    usr_password varchar(255) not null,
+    usr_active boolean,
+    PRIMARY KEY (usr_id)
+);
+
+create table dates(
+    dts_id int auto_increment,
+    dts_date varchar(255) not null,
+    dts_local varchar(255) not null,
+    dts_city_state varchar(255) not null,
+    dts_active boolean,
+    dts_record_by int not null,
+    PRIMARY KEY (dts_id),
+    FOREIGN KEY (dts_active) REFERENCES active(act_id),
+    FOREIGN KEY (dts_record_by) REFERENCES users(usr_id)
+);
+
+create table active(
+    act_id int auto_increment,
+    act_name varchar(255) not null,
+    PRIMARY KEY (act_id)
+);
---
+++ db/insert_tables.sql
@@ -0,0 +1,21 @@
+INSERT INTO users (usr_id, usr_name, usr_active)
+VALUES (1, 'adm', '1q2w3e4r',1);
+
+INSERT INTO users (usr_id, usr_name, usr_active)
+VALUES (2, 'thiago', 'noturnatall4ever' 1);
+
+INSERT INTO users (usr_id, usr_name, usr_active)
+VALUES (3, 'eliton','noturnatall4ever', 1);
+
+INSERT INTO dates(dts_id, dts_date, dts_local, dts_city_state, dts_active, dts_record_by)
+VALUES (1, '21/12/2013', 'Blackmore Rock Bar', 'São Paulo/SP', 1, 1)
+
+INSERT INTO dates(dts_id, dts_date, dts_local, dts_city_state, dts_active, dts_record_by)
+VALUES (2, '29/03/2014', 'Carioca Club', 'São Paulo/SP', 1, 1)
+
+INSERT INTO dates(dts_id, dts_date, dts_local, dts_city_state, dts_active, dts_record_by)
+VALUES (3, '24/04/2014', 'Assinatura Rock Walk', 'Limeira/SP', 1, 1)
+
+INSERT INTO dates(dts_id, dts_date, dts_local, dts_city_state, dts_active, dts_record_by)
+VALUES (4, '09/05/2014', 'LifeClub', 'Florianópolis/SP', 1, 1)
+
