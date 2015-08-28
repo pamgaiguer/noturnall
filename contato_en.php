8db7d80a676b7a35ff01dff135e6196abdfc28d9
Pamella Gaiguer <pamella.gaiguer@gmail.com>
2015-08-28 17:09:39 -0300

    Updating contact data

--- contato_en.php
+++ contato_en.php
@@ -16,13 +16,16 @@
 			<div class="col-xs-12 col-md-5">
 				<div id="contact">
 					<p>Contato para shows: <a href="mailto:show@noturnall.com">show@noturnall.com</a></p><br><br>
+					<p><a href="tel:+5511996371543">Telefone: +55 11 9 9637 1543</a></p><br>
+					<p>Assessoria: Fernanda Balbino &amp; Bribba Castro: <a href="mailto:fernanda.balbino@uol.com.br">fernanda.balbino@uol.com.br</a> e <a href="mailto:assessoria@bribbacastro.com.br">assessoria@bribbacastro.com.br</a></p>
+					<br>
 					<p>World Wide Management: </p>
 					<p>Brazil: Monika Cavalera for <a href="http://base2producoes.com.br/">Base 2 Productions</a> - <a href="mailto:monikacavalera@gmail.com">monikacavalera@gmail.com</a></p><br>
 					<p>Contato para impressa: Eliton Tomasi (Som do Darma): <a href="mailto:eliton@somdodarma.com.br">eliton@somdodarma.com.br</a> / <a href="tel:+551532111621">+55-15-3211-1621</a></p><br>
 					<p>Latin America: Karlos Alvarado for Agentur EAM - <a href="mailto:karlos@agentur-eam.de" target="_blank">karlos@agentur-eam.de</a></p><br>
 					<p>USA e Europe: Holger Koch for Metal Ville - <a href="mailto:holger@fd-entertainment-group.de" target="_blank">holger@fd-entertainment-group.de</a></p><br>
 					<p>Italy (Partner Collaboration) <a href="https://www.facebook.com/agentuream?fref=ts" target="_blank">Roberto Risso</a> - <a href="mailto:rob@agentur-eam.de">rob@agentur-eam.de</a></p><br>
-					<p>Official Audio and Video Studios: Fusão VM&T - <a href="http://www.fusaoestudios.com.br" target="_blank">www.fusaoestudios.com.br</a> / <a href="http://www.facebook.com/estudiofusao" target="_blank">www.facebook.com/estudiofusao</a> e Foggy: <a href="http://www.foggyfilmes.com">www.foggyfilmes.com</a></p><br>
+					<p>Official Audio and Video Studios: Fusão VM&amp;T - <a href="http://www.fusaoestudios.com.br" target="_blank">www.fusaoestudios.com.br</a> / <a href="http://www.facebook.com/estudiofusao" target="_blank">www.facebook.com/estudiofusao</a> e Foggy: <a href="http://www.foggyfilmes.com">www.foggyfilmes.com</a></p><br>
 					<p>Official Photo for "Back to Fuck You Up": <a href="https://www.facebook.com/fabioaugustofotografo" target="_blank">Fábio Augusto</a></p>
 					<p>Official Photo Team: Woody Studio - <A href="mailto:woody@studiowoody.com.br">woody@studiowoody.com.br</A> and Rafael Benitez Studios - <a href="mailto:contato@treearts.com">contato@treearts.com</a></p><br>
 					<p>Official Developer (Webmaster): Pamella Gaiguer - <a href="mailto:pamella.gaiguer@gmail.com">pamella@pamellagaiguer.com</a> e <a href="http://www.pamellagaiguer.com">http://www.pamellagaiguer.com</a></p><br>
